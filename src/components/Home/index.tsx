68220d8e8f44211a51b44be3a0d828d49834b5a3
Marcin <marcinkomar97@gmail.com>
2022-01-10 17:34:26 +0100

    implementacja chowania i pokazywania atrybutow

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,4 +1,3 @@
-import { useState } from 'react'
 import { UserData } from '../../types'
 import SpecificInfo from '../SpecificInfo'
 
@@ -7,12 +6,10 @@ interface Props {
 }
 
 const Home: React.FC<Props> = ({ userData }) => {
-  const [isShown, setIsShown] = useState(false)
   return (
     <table>
       <thead>
         <tr>
-          <th></th>
           <th>Imię</th>
           <th>Nazwisko</th>
           <th>Email</th>
@@ -23,15 +20,10 @@ const Home: React.FC<Props> = ({ userData }) => {
       <tbody>
         {userData.map((user, index) => (
           <tr key={index}>
-            <td>
-              <button type='button' onClick={() => setIsShown(!isShown)}>
-                Pokaż/Ukryj
-              </button>
-            </td>
             <td>{user.name}</td>
             <td>{user.surname}</td>
             <td>{user.email}</td>
-            {isShown ? <SpecificInfo userPrivateData={user} /> : null}
+            <SpecificInfo userPrivateData={user} />
           </tr>
         ))}
       </tbody>
--- src/components/SpecificInfo/index.tsx
+++ src/components/SpecificInfo/index.tsx
@@ -1,14 +1,25 @@
 import { UserPrivateData } from '../../types'
+import { useState } from 'react'
 
 interface Props {
   userPrivateData: UserPrivateData
 }
 
 const SpecificInfo: React.FC<Props> = ({ userPrivateData }) => {
+  const [isShown, setIsShown] = useState(false)
   return (
     <>
-      <td>{userPrivateData.password}</td>
-      <td>{userPrivateData.category}</td>
+      {isShown ? (
+        <>
+          <td>{userPrivateData.password}</td>
+          <td>{userPrivateData.category}</td>
+        </>
+      ) : null}
+      <td>
+        <button type='button' onClick={() => setIsShown(!isShown)}>
+          Pokaż/Ukryj
+        </button>
+      </td>
     </>
   )
 }
